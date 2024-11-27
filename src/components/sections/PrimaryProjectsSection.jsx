a992be273cae0a3b47aaa1adb148f683e8a56884
Jaime <jaime251102@gmail.com>
2024-11-27 02:18:22 -0400

    set bento on projects

--- src/components/sections/PrimaryProjectsSection.jsx
+++ src/components/sections/PrimaryProjectsSection.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
 import { BaseSection } from "../layout/BaseSection";
 
 export const PrimaryProjectsSection = () => {
   return (
     <BaseSection id='projects'>
+      <Link to='/projects' className="text-white italic">Ir a proyectos</Link>
     </BaseSection>
   );
 };
